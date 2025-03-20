b322e4d3f21813fc92058c23cf0651918951c75d
ProtikgoswamiCSE <goswami15-5841@diu.edu.bd>
2025-03-21 02:28:26 +0600

    21

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:transport_system/login/driver_user_login.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
