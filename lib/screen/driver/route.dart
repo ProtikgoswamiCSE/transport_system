6759a5d60ba5636edc0bbfef9754b4c25bef7d7e
taniatajin <tania15-4692@diu.edu.bd>
2025-04-09 00:20:22 +0600

    tajin

---
+++ lib/screen/driver/route.dart
@@ -0,0 +1,86 @@
+/*
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+class UrMapPage extends StatefulWidget {
+  const UrMapPage({super.key});
+
+  @override
+  State<UrMapPage> createState() => _UrMapPageState();
+}
+
+class _UrMapPageState extends State<UrMapPage> {
+  late GoogleMapController mapController;
+  Location location = Location();
+  LatLng? currentLocation;
+  bool _serviceEnabled = false;
+  PermissionStatus? _permissionGranted;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    try {
+      final locationData = await location.getLocation();
+      setState(() {
+        currentLocation =
+            LatLng(locationData.latitude!, locationData.longitude!);
+      });
+    } catch (e) {
+      print('Error getting location: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: currentLocation == null
+          ? const Center(child: CircularProgressIndicator())
+          : GoogleMap(
+              onMapCreated: (GoogleMapController controller) {
+                mapController = controller;
+              },
+              initialCameraPosition: CameraPosition(
+                target: currentLocation!,
+                zoom: 15.0,
+              ),
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              mapType: MapType.normal,
+              markers: {
+                Marker(
+                  markerId: const MarkerId('currentLocation'),
+                  position: currentLocation!,
+                ),
+              },
+            ),
+    );
+  }
+
+  @override
+  void dispose() {
+    mapController.dispose();
+    super.dispose();
+  }
+}
+*/
